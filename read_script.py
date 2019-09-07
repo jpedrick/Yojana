10289a4dae1145d42e3883c2bad4ed05f9b958ef
Joshua Pedrick <joshua.pedrick@gmail.com>
2019-09-07 13:22:31 -0400

    Only python dependencies, should work cross platform now.

--- read_script.py
+++ read_script.py
@@ -1,32 +1,41 @@
 #!/usr/bin/env python3
-import sys, os, argparse, hashlib, json, subprocess, time, io
+import sys, os, argparse, hashlib, json, time, io
+from urllib.request import urlopen
 
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+
+import pygame
+from pydub import AudioSegment
 import num2words
 from gtts import gTTS
 from num2words import num2words
 
-speech_dict = {}
+speech_dict = {} 
+sound_dict = {}
+
 devnull = open(os.devnull, 'w')
-cache_dir = "~/.yojana/cache"
+cache_dir = os.path.expanduser('~') + "/.yojana/cache"
 script_dir = '.'
+
+pygame.mixer.pre_init()
+pygame.mixer.init()
+pygame.init()
+
+sound_channel = pygame.mixer.find_channel()
+
 
 def wait_until( t ):
     while( t > time.time() ):
         time.sleep(0.1)
 
 def play_music(music_file, volume=0.8):
-    '''
-    stream music with mixer.music module in a blocking manner
-    this will stream the sound from disk while playing
-    '''
-    # set up the mixer
-    #freq = 44100     # audio CD quality
-    #bitsize = -16    # unsigned 16 bit
-    #channels = 2     # 1 is mono, 2 is stereo
-    #buffer = 2048    # number of samples (experiment to get best sound)
-    # volume value 0.0 to 1.0
+    if music_file not in sound_dict:
+        sound_dict[music_file] = pygame.mixer.Sound( AudioSegment.from_mp3( music_file ).export( format = 'ogg' ) )
     try:
-        subprocess.run( ["play", "--type", "mp3", "--volume", str(volume), '--buffer', '4096', '-V1', music_file ], stdout=devnull, stderr=devnull )
+        sound_channel.set_volume( volume )
+        sound_channel.queue( sound_dict[music_file] )
+        while sound_channel.get_busy():
+            time.sleep(0.1)
     except Exception as e:
         print("File {} not found! {}".format(music_file, str(e) ))
         return
@@ -43,11 +52,9 @@ def cache_words( words, cache_dir, voice = "en-in" ):
             error = gwav.save( c_path )
             speech_dict[ words ] = c_path
             time.sleep(0.1)
-            print( "saved \"", words, "\" to:", speech_dict[words], " with error: ", error )
     return speech_dict[ words ]
 
 def say_now( words, voice ):
-    print( words, end = ' ' )
     sys.stdout.flush()
 
     play_music( cache_words( words, cache_dir, voice=voice ) )
@@ -60,7 +67,10 @@ def do_sequence( sequence, script, start_at = 0 ):
         if start_at > 0:
             start_at = start_at - 1
             continue
-        print()
+
+        if "id" in i:
+            print( str(i["id"]) + ":", end = ' ' )
+
         if "sequence" in i:
             do_sequence( script["sequences"][ i["sequence"] ], script )
         else:
@@ -69,31 +79,41 @@ def do_sequence( sequence, script, start_at = 0 ):
                 end_et = i["duration"] + n_t
 
             if "sound" in i:
-                print('play sound: ', i["sound"], end=' ' )
+                print('play sound: ', i["sound"] )
                 play_music( '/'.join( [ script_dir, i["sound"] ] ) )
                 wait_until( end_et )
             elif "speech"  in i:
+                print( i["speech"] )
                 say_now(i["speech"], voice )
                 wait_until( end_et )
+            elif "sequences" in i:
+                for seq in i["sequences"]:
+                    do_sequence( script["sequences"][ seq ], script )
             elif "count_up"  in i:
                 print( "count up: ", end = ' ' )
                 for n in range(1, i["count_up"] + 1, 1):
                     n_t = time.time()
+                    print( num2words(n), end = ' ' )
                     say_now(num2words(n), voice )
                     wait_until( n_t + i["duration"])
+                print( )
             elif "count_down"  in i:
                 print( "count down: ", end = ' ' )
                 for n in range(i["count_down"],0, -1):
                     n_t = time.time()
+                    print( num2words(n), end = ' ' )
                     say_now(num2words(n), voice )
                     wait_until( n_t + i["duration"])
+                print( )
             elif "skip_count_down"  in i:
                 print( "skip count down: ", end = ' ' )
                 for n in range( i["skip_count_down"], 0, -i["duration"] ):
                     n_t = time.time()
+                    print( num2words(n), end = ' ' )
                     say_now(num2words(n), voice )
                     wait_until( n_t + i["duration"])
                 say_now("done", voice )
+                print( )
             elif "stop":
                 say_now("stop", voice )
                 return
@@ -105,7 +125,7 @@ parser.add_argument( '--skip', type=int )
 args = parser.parse_args()
 
 with open( args.script ) as script_file:
-    script_dir = os.path.dirname( args.script )
+    script_dir = os.path.dirname( os.path.abspath( args.script ) )
 
     script = json.load( script_file )
     voice = "en-in"
